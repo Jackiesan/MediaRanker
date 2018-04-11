38336efdea1ebeaf11f6e5f8a9ac02ffff2599da
Jackie <jonofre16@hotmail.com>
2018-04-11 15:29:47 -0700

    added User model and Sessions controller, updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get '/login', to: 'sessions#new', as: 'login_form'
+  post '/login', to: 'sessions#create', as: 'login'
+  delete '/login', to: 'sessions#destroy', as: 'logout'
+
+
   root 'works#main'
   resources :works
 end
