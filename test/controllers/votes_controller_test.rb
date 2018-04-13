e75f0c512a274f7136ad7928e3eff23bf0f2a1b8
Jackie <jonofre16@hotmail.com>
2018-04-13 13:24:43 -0700

    Added testing to methods and created fixture objects

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,9 +1,1 @@
 require "test_helper"
-
-describe VotesController do
-  it "should get create" do
-    get votes_create_url
-    value(response).must_be :success?
-  end
-
-end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
+
 describe Vote do
-  let(:vote) { Vote.new }
+  let(:vote_1) { votes(:vote_1) }
 
   it "must be valid" do
-    value(vote).must_be :valid?
+
+    value(vote_1).must_be :valid?
   end
+
+  it "must not be valid without a user" do
+    vote_1.user = nil
+    value(vote_1).wont_be :valid?
+    vote_1.errors.messages.must_include :user
+  end
+
+  it "must not be valid without a work" do
+    vote_1.work = nil
+    value(vote_1).wont_be :valid?
+    vote_1.errors.messages.must_include :work
+  end
+
+
 end
