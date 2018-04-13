57a9fdc01b6be81ca2f36185ba1bf392c60d6b76
Jackie <jonofre16@hotmail.com>
2018-04-13 14:49:52 -0700

    Added relational validation for Vote

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -3,9 +3,9 @@ require "test_helper"
 
 describe Vote do
   let(:vote_1) { votes(:vote_1) }
+  let(:vote_2) { Vote.new(user: users(:dan), work: works(:book_1)) }
 
   it "must be valid" do
-
     value(vote_1).must_be :valid?
   end
 
@@ -21,5 +21,32 @@ describe Vote do
     vote_1.errors.messages.must_include :work
   end
 
+  it "vote will not be created if user has already voted for that work" do
+    value(vote_2).wont_be :valid?
+  end
+
+  describe 'relations' do
+    let(:vote_2) { votes(:vote_2) }
+
+    it 'has a user' do
+      value(vote_2).must_be :valid?
+      vote_2.user.must_equal users(:chris)
+    end
+
+    it 'can set the user' do
+      vote_2.user = users(:dee)
+      vote_2.user.must_equal users(:dee)
+    end
+
+    it 'has a work' do
+      vote_2.work.must_equal works(:book_1)
+    end
+
+    it 'can set the work' do
+      vote_2.work = works(:movie_1)
+      vote_2.work.must_equal works(:movie_1)
+    end
+
+  end
 
 end
