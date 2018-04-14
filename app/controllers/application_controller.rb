4a429034112074c4142a2dac9464b751ca9cfba8
Jackie <jonofre16@hotmail.com>
2018-04-13 21:39:32 -0700

    Added application styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :find_user
 
   def find_user
     @user = User.find_by(id: session[:user_id])
